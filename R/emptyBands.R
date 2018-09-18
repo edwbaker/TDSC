0c2def3b33baf3002b1ffe4498e4288c4f9072a3
Ed Baker <edwbaker@gmail.com>
2018-09-18 20:56:25 +0100

    emptyBands() discovery. Closes #5.

---
+++ R/emptyBands.R
@@ -0,0 +1,27 @@
+#' Empty Bands Discovery
+#'
+#' Identifies unused codes across multiple S-matricies
+#' 
+#' @param ... Two or more TDSC objects
+#' @keywords TDSC
+#' @export
+#' @examples
+#' emptyBands()
+#' 
+emptyBands <- function(...) {
+  input <- list(...)
+  if (length(input) < 2) {
+    stop("emptyBands requires two or more TDSC objects")
+  }
+  x <- c()
+  y <- c()
+  for (i in 1:length(input)) {
+    s_matrix <- input[[i]]@s_matrix
+    x <- c(x, rep(i, length(s_matrix)))
+    y <- c(y, as.numeric(rownames(s_matrix)))
+  }
+  uy <- unique(y)
+  ay <- 1:max(uy)
+  m <- ay[!(ay %in% uy)]
+  return(list(missing=m, plot=cbind(x,y)))
+}
