c5d2fdd05eadb2c29c5b0ecd2ec5737595e41461
Ed Baker <edwbaker@gmail.com>
2018-09-18 19:38:49 +0100

    Include coding matrices from literature.

---
+++ R/chesmore2011-data.R
@@ -0,0 +1,21 @@
+#' Coding Matrix from Chesmore (2011)
+#'
+#' Coding matrix used by....
+#'
+#' @docType data
+#'
+#' @usage data(chesmore2011)
+#'
+#' @format Matrix
+#'
+#' @keywords TDSC CodingMatrix
+#'
+#' @references Moore et al. (2013) Genetics 195:1077-1086
+#' (\href{https://www.ncbi.nlm.nih.gov/pubmed/23979570}{PubMed})
+#'
+#' @source \href{https://phenome.jax.org/projects/Moore1b}{QTL Archive}
+#'
+#' @examples
+#' data(chesmore2011)
+#' t <- tdsc(wave, c_matrix=chesmore2011)
+"chesmore2011"
---
+++ R/farr2007-data.R
@@ -0,0 +1,21 @@
+#' Coding Matrix from Farr (2007)
+#'
+#' Coding matrix used by Farr in his 2007 PhD thesis.
+#'
+#' @docType data
+#'
+#' @usage data(farr2007)
+#'
+#' @format Matrix
+#'
+#' @keywords TDSC CodingMatrix
+#'
+#' @references Moore et al. (2013) Genetics 195:1077-1086
+#' (\href{https://www.ncbi.nlm.nih.gov/pubmed/23979570}{PubMed})
+#'
+#' @source \href{https://phenome.jax.org/projects/Moore1b}{QTL Archive}
+#'
+#' @examples
+#' data(farr2007)
+#' t <- tdsc(wave, c_matrix=farr2007)
+"farr2007"
