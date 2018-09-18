98a2cc37b55bcf5c9f79c0b31630478d55d55b84
Ed Baker <edwbaker@gmail.com>
2018-09-18 21:11:07 +0100

    followingCodes documentation. Fixes #10.

--- R/followingCodes.R
+++ R/followingCodes.R
@@ -1,7 +1,7 @@
 #' Following Codes
 #' #'
 #' Create sequences of codes that follow each other from time domain signal analysis
-#' @param codelist codelist form a TDSC object
+#' @param tdsc A TDSC object
 #' @param depth The length of the sequence of codes to search for
 #' @param min_code The minimum value of code to include in sequence
 #' @param max_code The maximum value of code to include in sequence
