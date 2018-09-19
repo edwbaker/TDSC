49fe8589f73449f4c8484ccdfb1ee3c31bd71d34
Ed Baker <edwbaker@gmail.com>
2018-09-19 16:01:14 +0100

    Functionming tests?

--- tests/testthat/test-emptyBands.R
+++ tests/testthat/test-emptyBands.R
@@ -1,11 +1,13 @@
 library(tuneR)
 context("empty bands")
 
-test_that("check  known missing bands in example file with chesmore2011 coding matrix", {
+test_that("check known missing bands in example file with chesmore2011 coding matrix", {
   wave <- readWave(system.file("extdata", "1.wav", package="tdsc"))
   data(chesmore2011)
   t <- tdsc(wave, coding_matrix = chesmore2011)
-  emptyBands(t,t)
-  test_that(emptyBands(t,t)$missing == 11)
+  b <- emptyBands(t,t)
+  expect_equal(b$missing, 11)
+  
+  expect_equal(1,1)
 })
 
