d5cacbe86a29e56b9eb43f6cd7a20d12242c14e0
Ed Baker <edwbaker@gmail.com>
2018-09-19 13:30:03 +0100

    Numerous changes to pass check().

--- R/c2dmatrix.R
+++ R/c2dmatrix.R
@@ -2,20 +2,20 @@
 #'
 #' Identifies unused codes across multiple S-matricies
 #' 
-#' @param input A tdsc object or a matrix
+#' @param t A tdsc object or a matrix
 #' @param sf The scaling factor
 #' @keywords TDSC
 #' @export
 #' @examples
-#' c2dmatrix(1)
+#' c2dmatrix(as.matrix(c(1,2,3,4), nrow=2))
 #' 
-c2dmatrix <- function(input, 
+c2dmatrix <- function(t, 
                       sf=100
 ) {
-  if (typeof(input) == "S4" & class(input)[[1]] == "tdsc") {
+  if (typeof(t) == "S4" & class(t)[[1]] == "tdsc") {
     c_matrix <- t@c_matrix
   } else {
-    c_matrix <- input
+    c_matrix <- t
   }
   for (i in 1:nrow(c_matrix)) {
     for (j in 1:ncol(c_matrix))
