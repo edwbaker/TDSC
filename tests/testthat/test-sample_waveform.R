aab3a4230936d7f146953b1271444176f03fb07a
Ed Baker <edwbaker@gmail.com>
2019-11-25 18:17:25 +0000

    Sample waveform tests

--- tests/testthat/test-sample_waveform.R
+++ tests/testthat/test-sample_waveform.R
@@ -2,5 +2,7 @@ context("sample waveform")
 
 test_that("parameters are of correct type", {
   wf <- sample_waveform()
-  expect_null(wf, "sample_waveofrm should return NULL")
+  expect_null(wf, "sample_waveform should return NULL")
+  wf <- sample_waveform(fig_max_samples=5, start_zero = FALSE,invert=TRUE, tdsc_shapes = TRUE,limit_y=FALSE)
+  expect_null(wf, "sample_waveform should return NULL")
 })
