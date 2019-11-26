645d61f83cf7ccf967a00e2922efb2a2285bee6d
Ed Baker <edwbaker@gmail.com>
2019-11-26 18:00:12 +0000

    Windowing namespace external functions.

--- R/tdsc.w.R
+++ R/tdsc.w.R
@@ -1,13 +1,13 @@
 tdsc.w <- function(start, wave=NULL, window.length=NULL) {
   remaining <- min(window.length, length(wave) + 1 - start) -1
-  w <- cutws(wave,from=start, to=start+remaining)
+  w <- sonicscrewdriver::cutws(wave,from=start, to=start+remaining)
   s <- tdsc(w)
   return(s)
 }
 
 tdsc.w.a <- function(start, wave=NULL, window.length=NULL) {
   remaining <- min(window.length, length(wave) + 1 - start) -1
-  w <- cutws(wave,from=start, to=start+remaining)
+  w <- sonicscrewdriver::cutws(wave,from=start, to=start+remaining)
   s <- tdsc(w)
   return(s@a_matrix)
 }
